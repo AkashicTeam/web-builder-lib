55d555028770e94b433a9fc3e62130a1b6d0655e
DoanChiQuang <chiquang127@gmail.com>
2024-10-16 19:05:39 +0700

    Config for using and testing package in other node

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+export * from './components/Button/index.js';
+
+const WebBuilderWrapper = () => {
+  return <div className="container">This is a layout</div>;
+};
+
+export default WebBuilderWrapper;
